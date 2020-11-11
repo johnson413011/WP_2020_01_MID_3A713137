f83c1911def949a36add56f31cdfff152457b7e1
Until Forever <Until Forever@UNTILFOREVER-PC>
2020-11-12 00:35:42 +0800

    加入專案檔案。

---
+++ WP_2020_01_MID_3A713137/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WP_2020_01_MID_3A713137
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.but = new System.Windows.Forms.Button();
+            this.tex = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Image = global::WP_2020_01_MID_3A713137.Properties.Resources._10;
+            this.pic.Location = new System.Drawing.Point(338, 152);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(422, 272);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // but
+            // 
+            this.but.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.but.Location = new System.Drawing.Point(338, 26);
+            this.but.Name = "but";
+            this.but.Size = new System.Drawing.Size(229, 105);
+            this.but.TabIndex = 1;
+            this.but.Text = "按鈕";
+            this.but.UseVisualStyleBackColor = true;
+            this.but.Click += new System.EventHandler(this.but_Click);
+            // 
+            // tex
+            // 
+            this.tex.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tex.Location = new System.Drawing.Point(25, 100);
+            this.tex.Multiline = true;
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(193, 95);
+            this.tex.TabIndex = 2;
+            this.tex.Text = "國家";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.but);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button but;
+        private System.Windows.Forms.TextBox tex;
+    }
+}
+
---
+++ WP_2020_01_MID_3A713137/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_MID_3A713137.Properties;
+
+namespace WP_2020_01_MID_3A713137
+{
+    public partial class Form1 : Form
+    {
+        List<Image> pc = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void but_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tex.Text))
+            {
+                MessageBox.Show("請輸入國家", "錯誤");
+                return;//重新檢查
+            }
+            else
+            {
+                switch (tex.Text)
+                {
+                    case "台灣":
+                        pic.Image = pc[0]; break;
+                    case "日本":
+                        pic.Image = pc[1]; break;
+                    case "菲律賓":
+                        pic.Image = pc[2]; break;
+                    case "印尼":
+                        pic.Image = pc[3]; break;
+                    case "泰國":
+                        pic.Image = pc[4]; break;
+                    case "中國":
+                        pic.Image = pc[5]; break;
+                    case "俄羅斯":
+                        pic.Image = pc[6]; break;
+                    case "美國":
+                        pic.Image = pc[7]; break;
+                    case "英國":
+                        pic.Image = pc[8]; break;
+                    case "韓國":
+                        pic.Image = pc[9]; break;
+                    default:
+                        MessageBox.Show("文字不符，請重新輸入國家", "錯誤");
+                        break;
+                }
+            }           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pc.Add(Resources._1);
+            pc.Add(Resources._2);
+            pc.Add(Resources._3);
+            pc.Add(Resources._4);
+            pc.Add(Resources._5);
+            pc.Add(Resources._6);
+            pc.Add(Resources._7);
+            pc.Add(Resources._8);
+            pc.Add(Resources._9);
+            pc.Add(Resources._10);
+        }
+    }
+}
